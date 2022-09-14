5ef4ce5bee4aa1cc956098aeb9cb33c782479d15
Excel951 <abigailexcelsisdeo@gmail.com>
2022-09-14 09:17:13 +0700

    Perubahan kedua

--- PBO221/m220914_1.java
+++ PBO221/m220914_1.java
@@ -1,7 +1,19 @@
 package PBO221;
 
+class User {
+    String username;
+    String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
+
 public class m220914_1 {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        User user1 = new User("username1", "password");
+        System.out.println("username: " + user1.username);
+        System.out.println("password: " + user1.password);
     }
 }
