2c1fd71e884814b592adb6c5cafadf5bcdd8c6c5
Excel951 <abigailexcelsisdeo@gmail.com>
2022-11-16 07:38:37 +0700

    Tugas M10

---
+++ PBO221/M221109/Tugas/AlatMusik.java
@@ -0,0 +1,25 @@
+package PBO221.M221109.Tugas;
+
+public class AlatMusik implements Transaksi {
+    String kategori;
+    int jumlahBeli, total;
+
+    @Override
+    public void payment() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void invoice() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public int stokUpdate() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
